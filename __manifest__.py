e90b7627ba9864e94108af66f60cc55fa1f83733
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-02-08 15:49:39 +0200

    edit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "ETMAM THEME", 
+    "summary": "ETMAM THEME",
+    "version": "13.0", 
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "template/assets.xml",
+    ],
+}
